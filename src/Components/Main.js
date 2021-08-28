6990feba73cbaccbcf92976aa12f5e6ab945e260
Saurav Datta <sauravdatta06@gmail.com>
2021-08-29 00:53:28 +0600

    Navbar update

---
+++ src/Components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import BurgerBuilder from './BurgerBuilder/BurgerBuilder';
+import Header from './Header/Header';
+
+
+const Main = () => {
+    return (
+        <div>
+            <Header />
+            <BurgerBuilder />
+        </div>
+    );
+}
+
+export default Main;
